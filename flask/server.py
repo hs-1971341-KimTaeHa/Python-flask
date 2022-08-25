a79c7accc9de69372a07ecbff757f934f27e10b2
unknown <xogk1128@naver.com>
2022-08-25 15:23:17 +0900

    read 구현

--- flask/server.py
+++ flask/server.py
@@ -3,25 +3,27 @@ import random
 app = Flask(__name__)
 
 topics = [
-    {'id':'1', 'title':'HTML', 'body':'HTML is ...'},
-    {'id':'2', 'title':'CSS', 'body':'CSS is ...'},
-    {'id':'3', 'title':'Javascript', 'body':'JS is ...'}
+    {'id':1, 'title':'HTML', 'body':'HTML is ...'},
+    {'id':2, 'title':'CSS', 'body':'CSS is ...'},
+    {'id':3, 'title':'Javascript', 'body':'JS is ...'}
 ]
 
-liTags=''
-for topic in topics:
-    liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+
 
 @app.route('/')
 def index():
+    liTags=''
+    for topic in topics:
+        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     return f'''
         <!doctype html>
+        <html>
             <head>
                 <meta charset="UTF-8">
                 <title>document</title>
             </head>
             <body>
-                <h1>WEB</h1>
+                <h1><a href="/">WEB</a></h1>
                 <ol>
                     {liTags}
                 </ol>
@@ -35,8 +37,33 @@ def index():
 def create():
     return 'create'
 
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/')
 def read(id):
-    return 'read'+id
+    liTags=''
+    for topic in topics:
+        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+    return f'''
+        <!doctype html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>document</title>
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    {liTags}
+                </ol>
+                <h2>{title}</h2>
+                {body}
+            </body>
+        </html>
+    '''
 
 app.run(debug=True)
