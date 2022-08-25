d11422a7ea41168e268671ec56e9a9c0a1aecd44
unknown <xogk1128@naver.com>
2022-08-25 15:31:08 +0900

    함수를 활용한 리팩토링

--- flask/server.py
+++ flask/server.py
@@ -8,62 +8,48 @@ topics = [
     {'id':3, 'title':'Javascript', 'body':'JS is ...'}
 ]
 
+def template(contents, content):
+    return f'''
+            <!doctype html>
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>document</title>
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        {contents}
+                    </ol>
+                    {content}
+                </body>
+            </html>
+        '''
 
+def getContens():
+    liTags=''
+    for topic in topics:
+        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
 
 @app.route('/')
 def index():
-    liTags=''
-    for topic in topics:
-        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-    return f'''
-        <!doctype html>
-        <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>document</title>
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    {liTags}
-                </ol>
-                <h2>Welcome</h2>
-                Hi...
-            </body>
-        </html>
-    '''
+    return template(getContens(), '<h2>Welcome</h2>Hi...' )
 
-@app.route('/create/')
-def create():
-    return 'create'
 
 @app.route('/read/<int:id>/')
 def read(id):
-    liTags=''
-    for topic in topics:
-        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     title = ''
     body = ''
     for topic in topics:
         if id == topic['id']:
             title = topic['title']
             body = topic['body']
-    return f'''
-        <!doctype html>
-        <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>document</title>
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    {liTags}
-                </ol>
-                <h2>{title}</h2>
-                {body}
-            </body>
-        </html>
-    '''
+    return template(getContens(), f'<h2>{title}</h2>{body}')
+
+@app.route('/create/')
+def create():
+    return 'create'
+
 
 app.run(debug=True)
